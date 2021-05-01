73ef15b7a7abd6c2f4d4b2b9341fe9ba1a451bb1
unknown <ashraf.habrumman@gmail.com>
2021-05-02 01:40:02 +0300

    transite data

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var allDataPoints = [], learningRate, activationFun, ratioOftrainingData, numberOfIteration, SSE
+var allDataPoints = [], learningRate, activationFun, ratioOftrainingData, numberOfIterations, SSE
 
 var currentDataColor, canvasWidth, canvasHeight
 
@@ -31,12 +31,29 @@ function mouseClicked(event) {
   if (mouseX > 0 && mouseX < canvasWidth && mouseY > 0 && mouseY < canvasHeight){
     var newPoint = new DataPoint(mouseX, mouseY, currentDataColor);
     allDataPoints.push();
-    console.log(newPoint.color)
+    // console.log(newPoint.color)
+    // console.log("learningRate"+learningRate)
   }
 }
-
 
 function setColor(color) {
   currentDataColor = color
 }
+
+function setLearningRate(learningRat) {
+  learningRate = learningRat
+}
+
+function setActivationFun(actfun) {
+  activationFun = actfun
+}
+
+function setIterationNumber(iteration) {
+  numberOfIterations = iteration;
+}
+
+function setRatioOfTrainingData(ratio) {
+  ratioOftrainingData = ratio;
+}
+
 // create a new instance of p5 and pass in the function for sketch 1
